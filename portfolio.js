6936ee856f1f7e89f818e960591801380451ab6e
helinam <helinarules@gmail.com>
2016-07-21 10:33:01 -0400

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -8,3 +8,11 @@ function myFunction() {
 		pic.src = "orange.jpg";
 	}
 }
+
+function init() {
+	var mapDiv = document.getElementById('map');
+        var map = new google.maps.Map(mapDiv, {
+            center: {lat: 39.8628, lng: 4.0273},
+            zoom: 8
+        });
+}
